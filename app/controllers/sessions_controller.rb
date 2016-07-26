e27295854a5623f02098a4d0917a9fada43eb961
Tatjana Nebel <tatjana.nebel@outlook.com>
2016-07-26 14:49:18 +0200

    secure password added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
   if user && user.authenticate(params[:session][:password])
     log_in user
-     redirect_to user
+     redirect_to home_path
     # Log the user in and redirect to the user's show page.
   else
     flash.now[:danger] = 'Invalid email/password combination'
