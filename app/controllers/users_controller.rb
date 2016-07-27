0c19afe7abfaf8752eb07022de97e06edc5f70c3
Tatjana Nebel <tatjana.nebel@outlook.com>
2016-07-27 09:46:10 +0200

    pg admin setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
   def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(
+    :username, :email,
+    :password, :password_confirmation
+    )
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,18 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+User.create(name: "Holly Winters", email: "holly.winters@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Dalton Delgado", email: "dalton.delgado@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Brock Cochran", email: "brock.cochran@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Sopoline Stanton", email: "sopoline.stanton@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Barry Thomas", email: "barry.thomas@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Gannon Hebert", email: "gannon.hebert@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Brittany Gillespie", email: "brittany.gillespie@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Rahim Yates", email: "rahim.yates@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Kylie Kent", email: "kylie.kent@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Cailin Dodson", email: "cailin.dodson@example.com", password: "123456", password_confirmation: "123456")
+User.create(name: "Michael Hartl", email: "michael.hartl@example.com", password: "123456", password_confirmation: "123456")
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
