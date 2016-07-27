aa3de51d807a3e561390fffc2fde70be7d6c7579
Tatjana Nebel <tatjana.nebel@outlook.com>
2016-07-27 16:46:54 +0200

    update model and migration soft_skills

---
+++ db/migrate/20160727142616_create_soft_skills.rb
@@ -0,0 +1,23 @@
+class CreateSoftSkills < ActiveRecord::Migration[5.0]
+  def change
+    create_table :soft_skills do |t|
+      t.string :career
+      t.string :importance
+      t.string :impact
+      t.string :super_power
+      t.string :help
+      t.string :like
+      t.string :dislike
+      t.string :greatest_achievment
+      t.string :best_friend
+      t.string :greatest_regret
+      t.string :motto
+      t.string :links_to_share
+      t.string :more_about
+      
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727090529) do
+ActiveRecord::Schema.define(version: 20160727142616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "soft_skills", force: :cascade do |t|
+    t.string   "career"
+    t.string   "importance"
+    t.string   "impact"
+    t.string   "super_power"
+    t.string   "help"
+    t.string   "like"
+    t.string   "dislike"
+    t.string   "greatest_achievment"
+    t.string   "best_friend"
+    t.string   "greatest_regret"
+    t.string   "motto"
+    t.string   "links_to_share"
+    t.string   "more_about"
+    t.integer  "user_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.index ["user_id"], name: "index_soft_skills_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
