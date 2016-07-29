13272b2bd1bb6eb000bce77d3d38936086992b66
Tatjana Nebel <tatjana.nebel@outlook.com>
2016-07-29 18:24:30 +0200

    mojitos by the beach

--- app/controllers/temp_soft_skills_controller.rb
+++ app/controllers/temp_soft_skills_controller.rb
@@ -4,7 +4,7 @@ class SoftSkillsController < ApplicationController
   end
   def soft_skills_params
     params.permit(
-    :career, :importance, :impact,:super_power, :help, :like, :dislike
+    :career, :importance, :impact,:super_power, :help, :like, :dislike,
     :greatest_achievement, :best_friend, :greatest_regret, :motto,
     :links_to_share, :more_about
     )
