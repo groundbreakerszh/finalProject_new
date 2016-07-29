e0620f72605ae2e9090d08c5121be436513e4e45
Tatjana Nebel <tatjana.nebel@outlook.com>
2016-07-29 18:21:42 +0200

    enjoying life

--- app/controllers/temp_soft_skills_controller.rb
+++ app/controllers/temp_soft_skills_controller.rb
@@ -5,7 +5,7 @@ class SoftSkillsController < ApplicationController
   def soft_skills_params
     params.permit(
     :career, :importance, :impact,:super_power, :help, :like, :dislike
-    :greatest_achievment, :best_friend, :greatest_regret, :motto,
+    :greatest_achievement, :best_friend, :greatest_regret, :motto,
     :links_to_share, :more_about
     )
   end
